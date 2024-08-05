0c5cc8bee5d2cef0cb3d7be1c2a22ecf9012f170
Adnan <adbak.dev@gmail.com>
2024-08-05 21:40:44 +0200

    styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Metadata } from "next";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { Provider } from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -16,9 +19,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body className={cn("min-h-screen font-sans antialiased", fontSans.variable)}>
+          <Provider>{children}</Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
