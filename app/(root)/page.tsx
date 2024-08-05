08ab69869fb3ae0850a8f4ec9b710b7b26a3d1ff
Adnan <adbak.dev@gmail.com>
2024-08-05 21:43:33 +0200

    liveblock use

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,38 @@
-import { Button } from "@/components/ui/button";
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+import Header from "@/components/Header";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-function Home() {
+async function Home() {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  const documents = [];
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      <div>
+        {documents.length > 0 ? (
+          <div></div>
+        ) : (
+          <div className="document-list-empty">
+            <Image src="/assets/icons/doc.svg" width={40} height={40} alt="Document icon" className="mx-auto" />
+            <AddDocumentBtn userId={user.id} email={user.emailAddresses[0].emailAddress} />
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
 
