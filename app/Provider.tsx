63620739ad8b4c2656f1b39d1c0befc713e51745
Adnan <adbak.dev@gmail.com>
2024-08-05 21:41:00 +0200

    provider

---
+++ app/Provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ReactNode } from "react";
+import { LiveblocksProvider, RoomProvider, ClientSideSuspense } from "@liveblocks/react/suspense";
+import Loader from "@/components/Loader";
+
+export function Provider({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+      <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
+    </LiveblocksProvider>
+  );
+}
