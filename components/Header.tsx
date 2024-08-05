1824e82d7f03686fdb359d33a66f020889652391
Adnan <adbak.dev@gmail.com>
2024-08-05 21:42:05 +0200

    loader + header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
-function Header({ children }: HeaderProps) {
+function Header({ children, className }: HeaderProps) {
   return (
-    <div className="header">
+    <div className={cn("header", className)}>
       <Link href="/" className="md:flex-1">
         <Image src="/assets/icons/logo.svg" alt="logo with name" width={120} height={32} className="hidden md:block" />
         <Image src="/assets/icons/logo.svg" alt="logo" width={120} height={32} className="mr-2 md:hidden" />
